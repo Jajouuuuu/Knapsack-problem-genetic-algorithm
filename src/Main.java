e215405f2c4d541218f68ec7d3b90c16a1567be4
PauloMalet <malet.paul@gmail.com>
2024-06-17 19:11:31 +0200

    Fix values list

--- src/Main.java
+++ src/Main.java
@@ -19,16 +19,23 @@ public class Main {
         System.out.println(sac);
         sac.supprimeObjet(1);
         System.out.println(sac);*/
-        List<Integer> maximumCost = List.of(100, 10, 70, 40, 30, 80, 90, 70, 75, 50);
+        List<Integer> maximumCost = List.of(200, 100, 700, 400, 300, 800, 900, 700, 750, 500);
         List<List<Integer>> costs = List.of(
                 List.of(95, 4, 60, 32, 23, 72, 80, 62, 65, 46),
-                List.of(2, 4, 6, 3, 2, 7, 8, 6, 6, 4),
+                List.of(50, 10, 20, 32, 23, 72, 8, 2, 50, 46),
+                List.of(30, 30, 10, 52, 43, 12, 50, 20, 20, 76),
+                List.of(90, 20, 90, 72, 83, 42, 60, 40, 50, 60),
+                List.of(50, 50, 50, 52, 53, 52, 50, 40, 50, 60),
+                List.of(90, 90, 90, 92, 93, 92, 90, 90, 90, 90),
+                List.of(40, 40, 40, 42, 43, 42, 40, 40, 40, 40),
+                List.of(70, 70, 70, 72, 73, 72, 70, 70, 70, 70),
+                List.of(2, 4, 6, 3, 2, 7, 8, 6, 6, 40),
                 List.of(950, 40, 600, 320, 230, 720, 800, 620, 650, 460)
         );
-        List<Integer> values = List.of(55, 10, 47);
+        List<Integer> values = List.of(55, 10, 47, 30, 20, 60, 90, 10, 78, 10);
         ApplicationGenetique algo = new ApplicationGenetique(maximumCost, costs, values);
-        int taillePopulation = 3;
-        int nombreDeGenerations = 5;
+        int taillePopulation = 10;
+        int nombreDeGenerations = 100;
         double mutationFactor = 0.1;
         List<Bag> resultats = algo.algorithmeGenetique(taillePopulation, nombreDeGenerations, mutationFactor);
         for (Bag bag : resultats) {
